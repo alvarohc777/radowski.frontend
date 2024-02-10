644895409a3e27f3d91af55d0d7d2960f04ddfa2
alvarohc777 <aherrada@uninorte.edu.co>
2024-02-10 11:05:04 -0500

    update blob token

--- src/stores/tokens-store.js
+++ src/stores/tokens-store.js
@@ -3,6 +3,6 @@ import { defineStore } from 'pinia'
 export const tokenStore = defineStore('token', {
   state: () => ({
     tokenAzure:
-      '?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-02-10T11:26:21Z&st=2024-01-23T03:26:21Z&spr=https&sig=mpe3aTH4DxOM8ZEDo0Loin8OBMnqvrjFUaPAXFVyNjs%3D'
+      '?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-05-31T00:02:45Z&st=2024-02-10T16:02:45Z&spr=https&sig=bQ%2FuEDbwXGMl%2BYxCKNUEuV7cEUmX8uUyMYyDA%2Buo08w%3D'
   })
 })
