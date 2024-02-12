27855ad977c09186af5f452f2e59184294badbaa
alvarohc777 <aherrada@uninorte.edu.co>
2024-02-12 00:59:29 -0500

    update token with less permissions

--- src/stores/tokens-store.js
+++ src/stores/tokens-store.js
@@ -3,6 +3,6 @@ import { defineStore } from 'pinia'
 export const tokenStore = defineStore('token', {
   state: () => ({
     tokenAzure:
-      '?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-05-31T00:02:45Z&st=2024-02-10T16:02:45Z&spr=https&sig=bQ%2FuEDbwXGMl%2BYxCKNUEuV7cEUmX8uUyMYyDA%2Buo08w%3D'
+      '?sv=2022-11-02&ss=bfqt&srt=sco&sp=r&se=2024-12-31T12:27:34Z&st=2024-02-12T04:27:34Z&spr=https&sig=fOf%2Fsnurp420W8P%2BPxPcbIR56eLCGJ8pu4OjXiWj8cY%3D'
   })
 })
